0364ff6aa06bd153cbd9e12cb6d4f3eccd234975
Tim Preston <134735@gmail.com>
2013-06-25 16:20:32 -0500

    add radio button, password box, etc to application layout

--- server_simple.rb
+++ server_simple.rb
@@ -10,8 +10,8 @@ server = WEBrick::HTTPServer.new :Port => 8000, :DocumentRoot => root
 ## Capture control+c to shut down the server
 trap 'INT' do server.shutdown end
 
-server.mount_proc '/' do |req, res|
-  res.body = 'Hello, world!'
+server.mount_proc '/internettest' do |req, res|
+  res.body = `ifconfig`
 end
 
 
