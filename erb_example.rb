38ee42af1b283d66e4c1100aaa42d3b5e96dc301
Tim Preston <134735@gmail.com>
2013-06-25 15:03:24 -0500

    Initial Commit

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,11 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+y = 'Alex'
+template2 = ERB.new "<h1><%= y%></h1>"
+puts template2.result(binding)
+
+z= 'www.google.com'
+template3 = ERB.new "<a href='<%= z %>'>the goog!</a>" 
+puts template3.result(binding)
